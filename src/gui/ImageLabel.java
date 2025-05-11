1d4458043ab0f6cb8ef05d9c250453d99558a241
aidascripca <aida-maria.scripca@student.tuiasi.ro>
2025-05-11 22:04:55 +0300

    Javadoc pt imagelabel

--- src/gui/ImageLabel.java
+++ src/gui/ImageLabel.java
@@ -11,6 +11,13 @@ public class ImageLabel extends GradientLabel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	/**
+	 * Constructor pentru ImageLabel:
+	 * initializeaza imaginile folosite si
+	 * seteaza vizibilitatea TRUE
+	 * @param text
+	 * implicit pentru ImageLabel: NULL
+	 */
 	public ImageLabel(String text) {
 		super(text);
 		initializarepoze();
@@ -18,14 +25,28 @@ public class ImageLabel extends GradientLabel {
 		// TODO Auto-generated constructor stub
 	}
 
-	
+	/**
+	 * icon pentru JLabel : umiditatea solului buna
+	 */
 	ImageIcon icnhappy;
+	/**
+	 * icon pentru JLabel: umiditatea solului scazuta
+	 */
 	ImageIcon icnsad;
+	/**
+	 * Variabila pentru contorizarea valorilor de 0 - sol uscat - transmise
+	 */
 	 int countOne = 0;
+	 /**
+	  *  Variabila pentru contorizarea valorilor de 1 - sol umed - transmise
+	  */
 	 int countZero = 0;
 	 
+	 /**
+	  * Functie pentru initializarea imaginilor respective nivelurilor de umiditate
+	  */
 	 public void initializarepoze()
-	    {
+	    { 
 	    	ImageIcon sad=new ImageIcon("src/gui/images/sad.png");
 	    	setOpaque(true);
 			setBounds(250, 75, 300, 300);
@@ -43,8 +64,20 @@ public class ImageLabel extends GradientLabel {
 			
 			
 	    }
-	 
+	 /**
+	  * Functie pentru comportarea interfatei in functie de umiditatea plantei:
+	  * Pentru 20 de valori de 0 consecutive, solul este uscat insa nu necesita atentie imediata
+	  * Pentru mai mult de 20 de valori de 0, solul necesita apa 
+	  * Pentru mai mult de 100 de valori de 0, planta este complet neglijata
+	  * 
+	  * pentru valori de 1, planta este OK, solul este umed
+	  * pentru prea multe valori de 1 se atentioneaza utilizatorul asupra udarii excesive
+	  * 
+	  * @param valoare
+	  *  valoarea citita de pe fisierul in care se incarca nivelul de umiditate - 0/1
+	  */
 	 @Override
+	
 	 public void setStatusValue(String valoare) {
 	    	
  	  
